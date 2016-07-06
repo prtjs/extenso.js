74b254d1692ae5336d6f9feb644f2a23fee16474
Matheus Alves <theuves@outlook.com>
2016-07-05 22:03:49 -0400

    ôxe

---
+++ test.js
@@ -0,0 +1,15 @@
+import test from 'ava';
+import extenso from './';
+
+test('extenso', t => {
+    t.is(extenso(123), 'cento e vinte e três');
+    t.is(extenso(-123), 'menos cento e vinte e três');
+    t.is(extenso('1000000'), 'um milhâo');
+    t.is(extenso('1.000.000'), 'um milhâo');
+    t.is(extenso('1,0'), 'um');
+    t.is(extenso('0,5'), 'cinco décimos');
+    t.is(extenso('1,5'), 'um inteiro e cinco décimos');
+    t.is(extenso(1, {feminino: true}), 'uma');
+    t.is(extenso(2, {feminino: true}), 'duas');
+    t.is(extenso('10000000000000001'), 'dez quatrilhões e um');
+});
