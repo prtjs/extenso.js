7051aca4620915e703c5d24c3386202f21426377
Matheus Alves <theuves@outlook.com>
2016-07-05 22:08:03 -0400

    corrige erro de digitação

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ import extenso from './';
 test('extenso', t => {
     t.is(extenso(123), 'cento e vinte e três');
     t.is(extenso(-123), 'menos cento e vinte e três');
-    t.is(extenso('1000000'), 'um milhâo');
-    t.is(extenso('1.000.000'), 'um milhâo');
+    t.is(extenso('1000000'), 'um milhão');
+    t.is(extenso('1.000.000'), 'um milhão');
     t.is(extenso('1,0'), 'um');
     t.is(extenso('0,5'), 'cinco décimos');
     t.is(extenso('1,5'), 'um inteiro e cinco décimos');
