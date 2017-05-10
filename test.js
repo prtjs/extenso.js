7879aaf25950e1ef769724e4ed5d7cbe9c694ca5
Matheus Alves <theuves@outlook.com>
2017-05-10 18:57:46 -0400

    usa dois espaços

--- test.js
+++ test.js
@@ -1,16 +1,18 @@
-import test from 'ava';
-import extenso from './';
+"use strict";
 
-test('extenso', t => {
-    t.is(extenso(123), 'cento e vinte e três');
-    t.is(extenso(-123), 'menos cento e vinte e três');
-    t.is(extenso(1000000), 'um milhão');
-    t.is(extenso('1000000'), 'um milhão');
-    t.is(extenso('1.000.000'), 'um milhão');
-    t.is(extenso('1,0'), 'um');
-    t.is(extenso('0,5'), 'cinco décimos');
-    t.is(extenso('1,5'), 'um inteiro e cinco décimos');
-    t.is(extenso(1, {feminino: true}), 'uma');
-    t.is(extenso(2, {feminino: true}), 'duas');
-    t.is(extenso('10000000000000001'), 'dez quatrilhões e um');
+var test = require("ava");
+var extenso = require("./");
+
+test('extenso', function (t) {
+  t.is(extenso(123), 'cento e vinte e três');
+  t.is(extenso(-123), 'menos cento e vinte e três');
+  t.is(extenso(1000000), 'um milhão');
+  t.is(extenso('1000000'), 'um milhão');
+  t.is(extenso('1.000.000'), 'um milhão');
+  t.is(extenso('1,0'), 'um');
+  t.is(extenso('0,5'), 'cinco décimos');
+  t.is(extenso('1,5'), 'um inteiro e cinco décimos');
+  t.is(extenso(1, {feminino: true}), 'uma');
+  t.is(extenso(2, {feminino: true}), 'duas');
+  t.is(extenso('10000000000000001'), 'dez quatrilhões e um');
 });
