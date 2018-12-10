3a38e1b97ba7418527fc6da1381cb3d49f69ed27
Matheus Alves <theuves@gmail.com>
2018-12-09 21:30:46 -0300

    correct the split test

--- src/gt1000/split/index.test.js
+++ src/gt1000/split/index.test.js
@@ -2,9 +2,9 @@ import test from 'ava'
 import fn from './'
 
 test('should split numbers', t => {
-  t.is(fn('1'), [ '1' ])
-  t.is(fn('10'), [ '10' ])
-  t.is(fn('100'), [ '100' ])
-  t.is(fn('1000'), [ '1', '000' ])
-  t.is(fn('1000000'), [ '1', '000', '000' ])
+  t.deepEqual(fn('1'), [ '1' ])
+  t.deepEqual(fn('10'), [ '10' ])
+  t.deepEqual(fn('100'), [ '100' ])
+  t.deepEqual(fn('1000'), [ '1', '000' ])
+  t.deepEqual(fn('1000000'), [ '1', '000', '000' ])
 })
