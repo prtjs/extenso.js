b2a89a72e836a731f937a6ff1b0571619441b1ba
Matheus Alves <theuves@gmail.com>
2018-12-12 11:17:45 -0300

    Traduz descrição para o português

--- src/gt1000/add-comma/index.test.js
+++ src/gt1000/add-comma/index.test.js
@@ -1,7 +1,7 @@
 import test from 'ava'
 import fn from './'
 
-test('should add commas between some parts', t => {
+test('Deve adicionar vírgula no final de algumas partes', t => {
   t.deepEqual(fn([ '1 milhão', 'mil e', '42' ]), [ '1 milhão,', 'mil e', '42' ])
   t.deepEqual(fn([ '1 milhão', 'mil', '420' ]), [ '1 milhão,', 'mil', '420' ])
 })
