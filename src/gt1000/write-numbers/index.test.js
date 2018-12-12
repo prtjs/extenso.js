22354db0ae278f9263161b14df13d2f8bd7863ec
Matheus Alves <theuves@gmail.com>
2018-12-12 13:21:05 -0300

    Adiciona testes para a função 'write-numbers'

---
+++ src/gt1000/write-numbers/index.test.js
@@ -0,0 +1,8 @@
+import test from 'ava'
+import fn from './'
+
+test('Deve escrever o restante dos números', t => {
+  t.deepEqual(fn([ '3 mil,', '140' ]), [ 'três mil,', 'cento e quarenta' ])
+  t.deepEqual(fn([ '3 mil e', '14' ]), [ 'três mil e', 'quatorze' ])
+  t.deepEqual(fn([ '3 mil e', '1' ]), [ 'três mil e', 'um' ])
+})
