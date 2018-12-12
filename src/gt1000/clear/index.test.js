df7819a7a76587889bfdfb2bfc845c7c69d2f0e1
Matheus Alves <theuves@gmail.com>
2018-12-12 11:18:50 -0300

    Traduz para o português e melhora os testes

--- src/gt1000/clear/index.test.js
+++ src/gt1000/clear/index.test.js
@@ -1,8 +1,10 @@
 import test from 'ava'
 import fn from './'
 
-test('should remove leading zeros and unread parts', t => {
-  t.deepEqual(fn([ '1 milhões', '000 mil', '000' ]), [ '1 milhões' ])
-  t.deepEqual(fn([ '1 milhões', '001 mil', '000' ]), [ '1 milhões', '1 mil' ])
-  t.deepEqual(fn([ '1 milhões', '001 mil', '001' ]), [ '1 milhões', '1 mil', '1' ])
+test('Deve remover zeros à esquerda', t => {
+  t.deepEqual(fn([ '2 milhões', '042 mil', '001' ]), [ '2 milhões', '42 mil', '1' ])
+})
+
+test('Deve remover partes que não são lidas', t => {
+  t.deepEqual(fn([ '2 milhões', '000 mil', '000' ]), [ '2 milhões' ])
 })
