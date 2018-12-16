db61ac1eb2a7dbfe7a90828bb2e64ba34a7ebe21
Matheus Alves <theuves@gmail.com>
2018-12-16 11:50:21 -0300

    Corrige alguns erros no estilo

--- src/gt1000/add-conjunction/index.test.js
+++ src/gt1000/add-conjunction/index.test.js
@@ -4,7 +4,7 @@ import fn from './'
 test('Deve adicionar "e" no final de algumas partes', t => {
   t.deepEqual(fn([ 'mil', '4' ], '1004'), [ 'mil e', '4' ])
   t.deepEqual(fn([ 'mil', '42' ], '1042'), [ 'mil e', '42' ])
-   t.deepEqual(fn([ 'mil', '200' ], '1200'), [ 'mil e', '200' ])
+  t.deepEqual(fn([ 'mil', '200' ], '1200'), [ 'mil e', '200' ])
 })
 
 test('Deve retornar o valor sem alterações', t => {
