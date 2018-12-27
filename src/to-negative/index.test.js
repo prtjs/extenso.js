f9a4bbe37a96f76b774f77276decb6a0f19c8e69
Matheus Alves <theuves@gmail.com>
2018-12-26 21:57:12 -0300

    Remove função 'to-negative'

--- src/to-negative/index.test.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava'
-import fn from './'
-
-test('Deve retornar a versão negativa em texto do número', t => {
-  t.is(fn('um'), 'um negativo')
-  t.is(fn('um', 'formal'), 'um negativo')
-  t.is(fn('um', 'informal'), 'menos um')
-})
