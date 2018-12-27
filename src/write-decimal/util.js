20e95781ab8b8f803fabe7ad17f8672215503419
Matheus Alves <theuves@gmail.com>
2018-12-26 21:57:46 -0300

    Informa nome do método

--- src/write-decimal/util.js
+++ src/write-decimal/util.js
@@ -1,6 +1,7 @@
 /**
  * Obter a informação do tipo da casa decimal (décimo ou centésimo).
  *
+ * @method getType
  * @param {number} place O número de casas do valor decimal.
  * @returns {string} Informação do tipo da casa.
  */
