2387676b46244192bc76ce0e08edfeff525f6942
Matheus Alves <theuves@gmail.com>
2018-12-29 00:05:38 -0300

    Refaz código para aceitar diferentes tipos de regiões

--- src/lt10.js
+++ src/lt10.js
@@ -1,12 +1,13 @@
-import { listLt10 as list } from './list'
+import { listLt10 as getList } from './get-list'
 
 /**
  * Obter um número inteiro menor que dez por extenso.
  *
  * @function lt10
  * @param {number} int Um número inteiro menor que dez.
+ * @param {string} locale Código do país para escrever o número.
  * @returns {string} O número por extenso.
  */
-export default (int) => {
-  return list[int]
+export default (int, locale) => {
+  return getList(locale)[int]
 }
--- src/lt100.js
+++ src/lt100.js
@@ -1,19 +1,20 @@
 import lt10 from './lt10'
-import { listLt100 as list } from './list'
+import { listLt100 as getList } from './get-list'
 
 /**
  * Obter um número inteiro menor que cem por extenso.
  *
  * @function lt100
  * @param {number} int Um número inteiro menor que cem.
+ * @param {string} locale Código do país para escrever o número.
  * @returns {string} O número escrito por extenso.
  */
-export default (int) => {
+export default (int, locale) => {
   if (int < 10) return lt10(int)
-  if (int < 20) return list[int - 10]
+  if (int < 20) return getList(locale)[int - 10]
 
   const unit = lt10(int % 10)
-  const ten = list[(int - int % 10) / 10 + 8]
+  const ten = getList(locale)[(int - int % 10) / 10 + 8]
 
   return unit !== 'zero'
     ? `${ten} e ${unit}`
