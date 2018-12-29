9f1a32d065ecd55caf7486b3b4d26dcb35b66929
Matheus Alves <theuves@gmail.com>
2018-12-29 10:03:53 -0300

    Corrige o chamamento da função

--- src/write-decimal/util.test.js
+++ src/write-decimal/util.test.js
@@ -2,10 +2,10 @@ import test from 'ava'
 import { getType } from './util'
 
 test('Deve retornar "décimo" ou "centésimo" para determinados números', (t) => {
-  t.is(fn(7), 'décimo')
-  t.is(fn(10), 'décimo')
-  t.is(fn(13), 'décimo')
-  t.is(fn(8), 'centésimo')
-  t.is(fn(11), 'centésimo')
-  t.is(fn(14), 'centésimo')
+  t.is(getType(7), 'décimo')
+  t.is(getType(10), 'décimo')
+  t.is(getType(13), 'décimo')
+  t.is(getType(8), 'centésimo')
+  t.is(getType(11), 'centésimo')
+  t.is(getType(14), 'centésimo')
 })
