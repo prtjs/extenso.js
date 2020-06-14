4e992b7884941a3894cdf1f047f654d9997872d1
Matheus Alves <theuves@gmail.com>
2020-06-14 17:55:25 -0400

    separa testes em mais blocos

--- src/num-util.test.js
+++ src/num-util.test.js
@@ -1,9 +1,6 @@
 import test from 'ava'
 import { isValidNumber, parseNumber } from './num-util'
 
-/**
- * Função: isValidNumber
- */
 test('Deve validar um número', (t) => {
   t.true(isValidNumber('42'))
   t.true(isValidNumber('42,0'))
@@ -14,16 +11,9 @@ test('Deve validar um número', (t) => {
   t.true(isValidNumber('1.000'))
   t.true(isValidNumber('1.234.567'))
   t.true(isValidNumber('1.234.567,42'))
+})
 
-  /*
-   * Number
-   */
-  t.true(isValidNumber(3.14))
-  t.true(isValidNumber(42))
-
-  /*
-   * Ponto como separador decimal
-   */
+test('Deve validar um número (separador decimal = .)', (t) => {
   t.true(isValidNumber('1000', true))
   t.true(isValidNumber('42.5', true))
   t.true(isValidNumber('1234567.42', true))
@@ -31,25 +21,25 @@ test('Deve validar um número', (t) => {
   t.true(isValidNumber('1,234,567.42', true))
 })
 
-/**
- * Função: parseNumber
- */
-test('Deve fazer um análise do número', (t) => {
+test('Deve validar um número (tipo number)', (t) => {
+  t.true(isValidNumber(3.14))
+  t.true(isValidNumber(42))
+})
+
+test('Deve analisar um número', (t) => {
   t.deepEqual(parseNumber('123'),        { isNegative: false, integer: '123', decimal: '0' })
   t.deepEqual(parseNumber('-123'),       { isNegative: true, integer: '123', decimal: '0' })
   t.deepEqual(parseNumber('123,42'),     { isNegative: false, integer: '123', decimal: '42' })
   t.deepEqual(parseNumber('-42.000,42'), { isNegative: true, integer: '42000', decimal: '42' })
+})
 
-  /*
-   * Float
-   */
-  t.deepEqual(parseNumber(3.14), { isNegative: false, integer: '3', decimal: '14' })
-
-  /*
-   * Ponto como separador decimal
-   */
+test('Deve analisar um número (separador decimal = .)', (t) => {
   t.deepEqual(parseNumber('123', true),        { isNegative: false, integer: '123', decimal: '0' })
   t.deepEqual(parseNumber('-123', true),       { isNegative: true, integer: '123', decimal: '0' })
   t.deepEqual(parseNumber('123.42', true),     { isNegative: false, integer: '123', decimal: '42' })
   t.deepEqual(parseNumber('-42,000.42', true), { isNegative: true, integer: '42000', decimal: '42' })
+})
+
+test('Dever analisar um número (tipo number)', (t) => {
+  t.deepEqual(parseNumber(3.14), { isNegative: false, integer: '3', decimal: '14' })
 })
