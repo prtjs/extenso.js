4ecc672e65f809b83b628f8bc98ac569a8e723a2
Arthur Santos <silva.arthur@academico.ifpb.edu.br>
2020-10-05 19:26:36 -0300

    Adiciona tipagem a exportação default

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+declare module 'extenso' {
+  /**
+   * Convert number to a extenze format
+   * @param number the number to convert in extenze text
+   * @param options setup writing configuration
+   */
+  export default function (
+    number: string | number,
+    options?: {
+      mode?: 'number' | 'currency'
+      locale?: 'pt' | 'br'
+      negative?: 'formal' | 'informal'
+      currency?: { type?: 'BRL' | 'EUR' | 'ECV' | 'MZN' | string }
+      number?: {
+        gender?: 'm' | 'f'
+        decimal?: 'formal' | 'informal'
+        decimalSeparator?: 'comma' | 'dot'
+      }
+    }
+  ): string
+}
