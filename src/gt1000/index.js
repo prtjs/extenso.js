a9b3d2e41f7fe74440ac46bfdd13d8eada175208
Matheus Alves <theuves@gmail.com>
2024-12-01 14:20:37 +0000

    Remove arquivos .js

--- src/gt1000/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const int_util_1 = require("./int-util");
-const parts_util_1 = require("./parts-util");
-exports.default = (int, locale, scale) => {
-    // TODO: Melhorar isso
-    const number = (0, parts_util_1.write)((0, parts_util_1.addComma)((0, parts_util_1.addConjunction)((0, parts_util_1.singularize)((0, parts_util_1.clear)((0, parts_util_1.name)((0, int_util_1.split)(int), locale, scale))), int)), locale);
-    return number.join(' ');
-};
--- src/lt10.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const get_list_1 = require("./get-list");
-exports.default = (int) => {
-    return (0, get_list_1.listLt10)()[int];
-};
