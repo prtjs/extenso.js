e3985d294ce139c140c73b800373483f68bfec0e
Matheus Alves <theuves@gmail.com>
2024-12-25 09:45:37 +0000

    Atualiza lower-than-10 e lower-than-100

--- src/core/lower-than-10.ts
+++ src/core/lower-than-10.ts
@@ -1,7 +1,7 @@
 import list from '../knowledge-base/lower-than-10'
 
-const lt10 = (input: number): string => {
-  return list[input]
+const lowerThan10 = (input: number): string => {
+    return list[input]
 }
 
-export default lt10
+export default lowerThan10
