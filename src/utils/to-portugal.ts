a1b2f67263233d2e51077cd8ce65f4212971ba5f
Matheus Alves <theuves@gmail.com>
2024-12-30 17:30:16 +0000

    Adiciona opção de Portgual para -ião e -iões

--- src/utils/to-portugal.ts
+++ src/utils/to-portugal.ts
@@ -6,6 +6,9 @@ const toPortugal = (text: string): string => {
         .replace(/\bdezenove\b/g, 'dezanove')
 
     return text
+        .replace(/[^m]ilh(ão|ões)/g, (suffix) => {
+            return suffix.replace(/h/g, 'i')
+        })
 }
 
 export default toPortugal
